8cd9670a1a196943a67649f661aaebc8da795b49
fikriyogi <fikriyogi@gmail.com>
2019-02-18 12:43:25 +0700

    Update and rename delete.php to index.php

--- ajax delete/index.php
+++ ajax delete/index.php
@@ -65,18 +65,3 @@ index.php
 </script>
 
 
-delete-data.php
-<?php
-include('core/db_connect.php');
-// $id = $_POST['id_dps'];
-//echo $music_number;
-if(isset($_GET['id_dps']))
-{
-$qry = "DELETE FROM dps WHERE id_dps =".$_GET['id_dps'];
-$result=mysqli_query($connect, $qry);
-if(isset($result)) {
-   echo "YES";
-} else {
-   echo "NO";
-}
-?>
