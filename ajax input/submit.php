e3278ffc4830431183fcdef95cc7a012f55ed874
fikriyogi <fikriyogi@gmail.com>
2019-02-18 12:37:50 +0700

    Create submit.php

---
+++ ajax input/submit.php
@@ -0,0 +1,45 @@
+index.php
+<?php if ($row['banned']==1) { echo "<button class='btn btn-danger btn-sm remove'>Banned</button>"; } else { echo "<button class='btn btn-success btn-sm remove'>Aktif</button>"; } ?>
+
+
+<!-- Ajax Delete -->
+<script type="text/javascript">
+	  $(".remove").click(function(){
+        var id = $(this).parents("tr").attr("id");
+
+
+        if(confirm('Are you sure to remove this record ?'))
+        {
+            $.ajax({
+               url: 'banned-users.php',
+               type: 'GET',
+               data: {id: id},
+               error: function() {
+                  alert('Something is wrong');
+               },
+               success: function(data) {
+                    $("#"+id).remove();
+                    alert("Record removed successfully");  
+               }
+            });
+        }
+    });
+
+</script>
+
+
+
+banned-user.php
+<?php
+$mysqli = new mysqli("localhost", "root", "", "user-management");
+// $id = $_POST['user_id'];
+//echo $music_number;
+
+if(isset($_GET['id']))
+{
+     $sql = "UPDATE `users` SET `banned` = '1' WHERE `users`.`id`=".$_GET['id'];
+     $mysqli->query($sql);
+	 echo 'Deleted successfully.';
+}
+
+
